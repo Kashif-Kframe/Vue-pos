3dacfb3c702a6af8888657445a08636bec9fbaa9
Kashif Sohail <kashif@mawqiftech.com>
2023-10-28 21:43:54 +0500

    add home page

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
+      <router-link to="/">Home</router-link>
       <router-link to="/company">Companies</router-link>
       <router-link to="/employee">Employees</router-link><br>
     </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 import Company from './components/Company.vue'
 import Employee from './components/Employee.vue'
+import Home from './components/Home.vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router';
@@ -8,6 +9,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/',
+            component: Home
+        },
         {
             path: '/company',
             component: Company
